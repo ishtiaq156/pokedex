6429bcf93de8d910578d9227cd123727800fac4e
Ishtiaq Ul Haq Syed <ishtiaq156@gmail.com>
2025-10-21 17:00:38 -0500

    update image URLs to use raw GitHub content and add Hisuian forms for several Pokémon

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
         pathname:
           "/gh/PokeMiners/pogo_assets@master/Images/Pokemon%20-%20256x256/**",
       },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname:
+          "/PokeMiners/pogo_assets/master/Images/Pokemon%20-%20256x256/**",
+      },
     ],
     // Reduce transformations by limiting formats to webp only
     formats: ["image/webp"],
